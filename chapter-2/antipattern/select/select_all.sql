47c6d5c6c98f09a50a08af25da99f8151e06b836
nawawan <mizutake0428@yahoo.co.jp>
2025-04-21 00:47:55 +0900

    add depth information for select statement

--- chapter-2/antipattern/select/select_all.sql
+++ chapter-2/antipattern/select/select_all.sql
@@ -1,10 +1,10 @@
 WItH RECURSIVE
     come AS (
-        SELECT c1.comment_id
+        SELECT c1.comment_id, 0 depth
         FROM Comments c1
         WHERE c1.parent_id IS NULL
         UNION ALL
-        SELECT c3.comment_id
+        SELECT c3.comment_id, come.depth + 1
         FROM come INNER JOIN Comments c3
         ON c3.parent_id = come.comment_id
     )
